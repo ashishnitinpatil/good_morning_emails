5c5ba50d63227e743625827febde17686e84840d
Ashish Patil <ashishnitinpatil@gmail.com>
2016-03-23 17:49:18 +0530

    +utilities, +requirements

---
+++ utilities.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+import os
+import json
+import time
+import requests
+
+
+# suppress dirty SSL warnings when using requests
+try:
+    requests.packages.urllib3.disable_warnings()
+except AttributeError:
+    pass
+
+
+def timestamp():
+    """Returns YYYY-MM-DD HH:MM:SS formatted UTC timestamp string"""
+    return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+
+
+def get_public_ip():
+    """Fetch public IP of hosting server with the help of httpbin API"""
+    return json.loads(requests.get('http://httpbin.org/ip').text)['origin']
+
+
+def send_email(subject="", html="", from_id="", recipients=[], debug=False):
+    """Send an email with the given html body & subject line with the help of MAILGUN API"""
+    sandbox = os.environ.get('MAILGUN_SANDBOX', '')
+    request_url = 'https://api.mailgun.net/v3/{0}/messages'.format(sandbox)
+    request = requests.post(
+        request_url,
+        verify=False,
+        auth=('api', os.environ.get('MAILGUN_KEY', '')),
+        data={
+            'from': from_id,
+            'to': recipients,
+            'subject': subject,
+            'html': html,
+        },
+    )
+    if debug:
+        print 'Status: {0}'.format(request.status_code)
+        print 'Body:   {0}'.format(request.text)
+    return True if int(request.status_code) == 200 else False
+
+
+__all__ = ['timestamp', 'get_public_ip', 'send_email']
